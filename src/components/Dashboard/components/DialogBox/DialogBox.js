eb321aa345295b941952a48cf4af812abb679221
totti1 <stotestorch@gmail.com>
2018-04-27 12:02:45 +0200

    new modification on dashboard

--- src/components/Dashboard/components/DialogBox/DialogBox.js
+++ src/components/Dashboard/components/DialogBox/DialogBox.js
@@ -9,6 +9,55 @@ import Dialog, {
   DialogContentText,
   DialogTitle,
 } from 'material-ui/Dialog';
+import SvgIcon from 'material-ui/SvgIcon';
+import Paper from 'material-ui/Paper';
+
+import styles from './style'
+
+const ScreenHeight= window.innerHeight
+const ScreenWidth= window.innerWidth
+
+const stylish = {
+
+  categoryBtn:{
+     // backgroundColor: '#005464',
+    borderRadius: 5,
+    border: 'groove',
+    borderColor: '#005464',
+    // marginLeft: -200,
+    width: ScreenWidth/4
+  },
+  btnBuy:{
+    // backgroundColor: '#005464',
+    borderRadius: 5,
+    // border: 'groove',
+    borderColor: '#005464',
+    color: '#005464',
+    fontWeight: 'bold',
+    '&:active': {
+      boxShadow: 'none',
+      backgroundColor: '#0062cc',
+      borderColor: '#005cbf',
+    },
+    '&:hover, &$focusVisible': {
+      backgroundColor: '#005464',
+      borderRadius: 5,
+      // width: 300,
+      color: 'white',
+      fontWeight: 'bold',
+  },
+    width: ScreenWidth/8,
+  },
+  btnSell:{
+    // backgroundColor: '#005464',
+    borderRadius: 5,
+    // border: 'groove',
+    borderColor: '#005464',
+    color: '#005464',
+    fontWeight: 'bold',
+    width: ScreenWidth/8
+  },
+}
 
 class DialogBox extends React.Component {
   state = {
@@ -25,15 +74,47 @@ class DialogBox extends React.Component {
 
   render() {
 
-    const { classes, title, onChangeCurrency, onChangeRate, buttonTxt, buttonTxt2, onClick, onClickCancel } = this.props;
+    const { 
+      classes, 
+      title, 
+      onChangeCurrency, 
+      onChangeRate, 
+      buttonTxt, 
+      buttonTxt2, 
+      onClick, 
+      onClickCancel,
+      onClickBuy,
+      onClickSell, 
+      description } = this.props;
 
     return (
       <div>
-          <DialogTitle id="form-dialog-title">Add Currency</DialogTitle>
+          <DialogTitle id="form-dialog-title">{title}</DialogTitle>
           <DialogContent>
             <DialogContentText>
-              {title}
+              {description}
             </DialogContentText>
+            {/* <DialogActions> */}
+            <Paper 
+              style={stylish.categoryBtn} 
+              elevation={0}>
+
+              <Button 
+                  style={stylish.btnBuy}
+                  // className={classes.btnBuy}
+                  onClick={onClickBuy}
+              >
+                Buy
+              </Button>
+              <Button 
+                  style={stylish.btnSell}
+                  // className={classes.btnSell}
+                  onClick={onClickSell}
+              >
+                Sell
+              </Button>
+            </Paper>
+          {/* </DialogActions> */}
             <TextField
               autoFocus
               margin="dense"
@@ -54,10 +135,18 @@ class DialogBox extends React.Component {
             />
           </DialogContent>
           <DialogActions>
-            <Button onClick={onClick} color="primary">
+            <Button onClick={onClick} color="primary" className={classes.button}>
+            <SvgIcon 
+                style={{color: 'white', fontSize: 40, marginRight: 20}}>
+              <path d="M17,13H13V17H11V13H7V11H11V7H13V11H17M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3Z" />
+            </SvgIcon>
               {buttonTxt}
             </Button>
-            <Button onClick={onClickCancel} color="primary">
+            <Button onClick={onClickCancel} color="primary" className={classes.buttonCancel}>
+            <SvgIcon 
+                style={{color: 'white', fontSize: 40, marginRight: 20}}>
+              <path d="M13,13H11V7H13M13,17H11V15H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
+            </SvgIcon>
               {buttonTxt2}
             </Button>
           </DialogActions>
@@ -69,13 +158,15 @@ class DialogBox extends React.Component {
 DialogBox.propTypes = {
   classes: PropTypes.object.isRequired,
   title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
   onChangeCurrency: PropTypes.func,
   onChangeRate: PropTypes.func,
   buttonTxt: PropTypes.string.isRequired,
   buttonTxt2: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
-  onClickCancel: PropTypes.func.isRequired
+  onClickCancel: PropTypes.func.isRequired,
+  onClickBuy: PropTypes.func.isRequired,
+  onClickSell: PropTypes.func.isRequired
 };
 
-// export default withStyles(styles) (DialogBox)
-export default DialogBox
+export default withStyles(styles) (DialogBox)
